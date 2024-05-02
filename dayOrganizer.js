b84e42cd56edf850dd1484b9a6bbfb6de9d7ba72
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2024-05-02 15:20:04 +0300

    ===

--- dayOrganizer.js
+++ dayOrganizer.js
@@ -21,7 +21,7 @@ function moveToDay() {
                 }
             } else if (extractDeviceName(userAgent) == studySessionData.userDevice) {
                 if (studySessionData.doneInstructions === "doneInstructions") {
-                    if ((todayDate >= "20") && (studySessionData.doneTest1 == "")) { //NO ZEROS IN FRONT OF SINGEL DIGITS
+                    if (((todayDate >= "20") || (Number(todayDate) === 20)) && (studySessionData.doneTest1 == "")) { //NO ZEROS IN FRONT OF SINGEL DIGITS
                         platform.goToUrl("days/twoTests/twoTests.html");
                     } else if
                         (
